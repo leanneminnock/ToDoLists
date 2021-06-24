9a0e6d13542164e0afa3fb1b80a4c9338438aaf5
Leanne Minnock <leanneminnock91@gmail.com>
2021-06-24 20:18:08 +0100

    - created form component for new lists

---
+++ src/app/create-list-form/create-list-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-list-form',
+  templateUrl: './create-list-form.component.html',
+  styleUrls: ['./create-list-form.component.css']
+})
+export class CreateListFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
